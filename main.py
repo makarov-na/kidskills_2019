c12fccb3ea39066e7e6cae631460a76a026532e7
Nikolay Makarov <makarov-na@yandex.ru>
2019-09-18 23:30:08 +0300

    Gyro sensor

--- main.py
+++ main.py
@@ -5,36 +5,31 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
-from grabber_module import Grabber
 from truck_module import Truck
 
 #Инициализация
-grabber = Grabber(Motor(Port.A))
-truck = Truck(Motor(Port.B),Motor(Port.C))
-front_sensor = UltrasonicSensor(Port.S1)
+
+truck = Truck(Motor(Port.B),Motor(Port.C), Motor(Port.A), UltrasonicSensor(Port.S1))
 left_sensor = ColorSensor(Port.S2)
 right_sensor = ColorSensor(Port.S3)
 
 
 #Работа
 
-def takeObject:
-    grabber.open()
-    truck.run_forward()
-    while (front_sensor.distance()>40):
-        print(front_sensor.distance())
-        wait(10)
-    grabber.close()
+'''
+while (True):
+    print("LR: ", left_sensor.reflection(), "RR: ", right_sensor.reflection())
+    wait(1000)
+'''
+truck.rotate_180()
 
-
-takeObject()
-truck.rotate180()
+'''
+truck.take_object()
+truck.rotate_180()
 truck.run_forward()
-wait(5000)
-
 truck.stop()
-grabber.open()
-
+truck.open_grubber()
+'''
 brick.sound.beep()
 
 
--- truck_module.py
+++ truck_module.py
@@ -5,13 +5,16 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
+from grabber_module import Grabber
 
 class Truck:
 
-    def __init__ (self, left_motor, right_motor):
+    def __init__ (self, left_motor, right_motor, grabber_motor, front_sensor):
         self.TRUCK_SPEED = 500;
         self.left_motor = left_motor
         self.right_motor = right_motor
+        self.grabber = Grabber(grabber_motor)
+        self.front_sensor = front_sensor
 
     def run_forward(self):
         self.left_motor.run(self.TRUCK_SPEED)
@@ -25,9 +28,27 @@ class Truck:
         self.left_motor.run(-self.TRUCK_SPEED)
         self.right_motor.run(-self.TRUCK_SPEED)
 
-    def rotate180(self):
-        ROTATE_ANGLE = 490
-        self.left_motor.run_angle(500, ROTATE_ANGLE, Stop.COAST, False)
-        self.right_motor.run_angle(500, -ROTATE_ANGLE, Stop.COAST, False)
+    def rotate_180(self):
+        gyro_sensor = GyroSensor(Port.S4)
+        gyro_sensor.reset_angle(0)
+        self.left_motor.run(self.TRUCK_SPEED)
+        self.right_motor.run(-self.TRUCK_SPEED)
+        while (gyro_sensor.angle()<135):
+            print("Angle: ",gyro_sensor.angle())
+            wait(1)
+        self.stop()
         wait(4000)
+    
+    def take_object(self):
+        self.grabber.open()
+        self.run_forward()
+        while (self.front_sensor.distance()>40):
+            print("Distance to object: ",self.front_sensor.distance())
+            wait(1)
+        self.stop()
+        self.grabber.close()
+        
+
+    def open_grubber(self):
+        self.grabber.open()
 
