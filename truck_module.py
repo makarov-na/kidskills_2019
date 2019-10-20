b534e06e098a1148098a4d5f21fb11c6f79f6813
Nikolay Makarov <makarov-na@yandex.ru>
2019-10-20 13:48:18 +0300

    Вернул журналирование

--- truck_module.py
+++ truck_module.py
@@ -101,7 +101,7 @@ class PidRegulator:
         self.kd = kd
         self.ki = ki
         self.integral_value_max = 20
-    
+
     def get_current_error(self):
         return self.current_err
 
@@ -114,7 +114,7 @@ class PidRegulator:
             self.integr_err = -self.integral_value_max
         self.diff_err = self.current_err - self.prevent_err
         output_val = self.kp*self.current_err + self.kd*self.diff_err + self.ki*self.integr_err
+        print("target_value: ", self.target_value, " current_value: ", current_value, " current_err: ", self.current_err,
+              "prevent_err: ", self.prevent_err, " integr_err: ", self.integr_err, "diff_err: ", self.diff_err, "optput: ", output_val)
         self.prevent_err = self.current_err
-        '''print("target_value: ", self.target_value, " current_value: ", current_value, " current_err: ", current_err,
-              "prevent_err: ", self.prevent_err, " integr_err: ", self.integr_err, "diff_err: ", self.diff_err, "optput: ", output_val)'''
         return output_val
