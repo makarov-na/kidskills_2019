4901b054b217d6c7ec5e13205645284dc6a943cf
Nikolay Makarov <makarov-na@yandex.ru>
2019-10-26 23:22:21 +0300

    Метод для тестирования максимальной скорости исправление текста

--- util_module.py
+++ util_module.py
@@ -16,10 +16,9 @@ def on_junction_or_turn_90(main_sensor, add_sensor, line_side_reflection):
     return (add_sensor.reflection() < line_side_reflection) and (main_sensor.reflection() < line_side_reflection + main_sensor_deviation)
 
 def calibrate_line_side(sensor):
-    return 30
     brick.display.clear()
     brick.display.text("Position left sensor")
-    brick.display.text("to line side ")
+    brick.display.text("to left line border ")
     brick.display.text("and press any key")
     while not any(brick.buttons()):
         wait(10)
@@ -27,3 +26,9 @@ def calibrate_line_side(sensor):
     line_side = sensor.reflection()
     return line_side
 
+def test_max_speed():
+    while True:
+        truck.drive(320, 320)
+        print("speed: " + str(steering), "L motor speed: ", left_wheel_motor.speed(), "R motor speed: ", right_wheel_motor.speed())
+        steering = steering + 1 
+        wait(10)
